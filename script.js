66ea4119130067fc1cff89332af0db5ef90f285b
Code-With-Bhavya <guptabhavya1402@gmail.com>
2024-07-11 17:20:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const API_KEY = ['AIzaSyAf0edYw94yUvMuLmEf5CVUw11XrpHT_5U','AIzaSyCGRQ9QiwOsZdbaUG6I0tFIUdsdfdOx9YE' , 'AIzaSyBRG6SMwIu4JrZM_KXJbU_SWL-u_X0ToWQ'] // Replace with your API key
-const CHANNEL_ID = 'UCZmAh77ShJkCPUp-Nl0PLOw';  // Replace with your channel ID
+const API_KEY = ['AIzaSyAf0edYw94yUvMuLmEf5CVUw11XrpHT_5U','AIzaSyCGRQ9QiwOsZdbaUG6I0tFIUdsdfdOx9YE' , 'AIzaSyBRG6SMwIu4JrZM_KXJbU_SWL-u_X0ToWQ' , 'AIzaSyAvymR0iK-HJT3el82J4pqaCCAsLdfGTY8' , 'AIzaSyBeetgOLmBUZZyu5mDoqyYHYNu5AOg_ZJw' , 'AIzaSyDgfSmkaCtIh0yOdkyDke_vm7yYekxxJII' , 'AIzaSyB0mNeefxpygxDi9BHo8az4T7HW2RlDJ5s' , 'AIzaSyCFPUfeGgz-ENmUY16qypYHPYj8NglYDT4' , 'AIzaSyAETzrtqnIFQ2VrvDLrWUapoyXN-8k1I50' , 'AIzaSyC8mo_BZz01zzPFDDas7gixCsnj8I2EXuQ' , 'AIzaSyBEKVTxfu6eWEdpBYqeOnlbkGRaYUxrpN4' , 'AIzaSyD0mFcvcsMt9ff0j5JaSK4yH7pisalHGLM'] // Replace with your API key
+const CHANNEL_ID = 'UCZmAh77ShJkCPUp-Nl0PLOw';
 
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -38,7 +38,7 @@ function toggleSidebar() {
 
 async function fetchSubscriberCount() {
     try {
-        const response = await fetch(`https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${CHANNEL_ID}&key=${API_KEY[getRandomInt(0,3)]}`);
+        const response = await fetch(`https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${CHANNEL_ID}&key=${API_KEY[getRandomInt(0, API_KEY.length)]}`);
         if (!response.ok) {
             throw new Error(`HTTP error! Status: ${response.status}`);
         }
@@ -56,7 +56,7 @@ async function fetchSubscriberCount() {
 }
 async function fetchTotalViews() {
     try {
-        const response = await fetch(`https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${CHANNEL_ID}&key=${API_KEY[getRandomInt(0,3)]}`);
+        const response = await fetch(`https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${CHANNEL_ID}&key=${API_KEY[getRandomInt(0, API_KEY.length)]}`);
         if (!response.ok) {
             throw new Error(`HTTP error! Status: ${response.status}`);
         }
@@ -75,7 +75,7 @@ async function fetchTotalViews() {
 }
 async function fetchShortViews() {
     try {
-        const shortsResponse = await fetch(`https://www.googleapis.com/youtube/v3/search?order=date&part=snippet&channelId=${CHANNEL_ID}&maxResults=9&type=video&key=${API_KEY[getRandomInt(0,3)]}`);
+        const shortsResponse = await fetch(`https://www.googleapis.com/youtube/v3/search?order=date&part=snippet&channelId=${CHANNEL_ID}&maxResults=9&type=video&key=${API_KEY[getRandomInt(0,API_KEY.length)]}`);
         const shortsData = await shortsResponse.json();
         const shorts = shortsData.items;
         const shortsContainer = document.querySelector('.shorts');
@@ -86,14 +86,25 @@ async function fetchShortViews() {
             const title = short.snippet.title;
             const viewsResponse = await fetch(`https://www.googleapis.com/youtube/v3/videos?part=statistics&id=${videoId}&key=${API_KEY[getRandomInt(0,3)]}`);
             const viewsData = await viewsResponse.json();
-            const views = viewsData.items[0].statistics.viewCount;
+            const viewnum = viewsData.items[0].statistics.viewCount;
+            function formatNumber(num) {
+                if (num >= 1000) {
+                    return (num / 1000).toFixed(1) + 'K';
+                }
+                return num.toString();
+            };
+            const views = formatNumber(viewnum);
             const shortElement = document.createElement('div');
             shortElement.className = 'short';
             shortElement.innerHTML = `
                     <a href="https://youtube.com/watch?v=${videoId}" target="_blank">
-                        <img src="${thumbnailUrl}" alt="${title}">
-                        <p>${title}</p>
-                        <div class="views">Views: ${views}</div>
+                        <img src="${thumbnailUrl}" id='shortimg' alt="${title}">
+                        <div id='shortcontent'>
+                            <img src="view.png" id='viewicon' alt=''>
+                            <span class="views"> ${views}</span>
+                        </div>
+                        <p class='shorttitle'>${title}</p>
+                        
                     </a>`;
             shortsContainer.appendChild(shortElement);
         });
